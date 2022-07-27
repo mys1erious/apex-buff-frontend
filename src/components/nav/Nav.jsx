772c4578b4df908c7eeff0713210c134de39356f
Yurii <yuriibel888@gmail.com>
2022-07-27 20:18:27 +0300

    placeholder fixed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -42,7 +42,7 @@ const Nav = () => {
                       type="search"
                       id="usernameSearch"
                       className="form-control relative flex-auto min-w-0 block w-full px-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                      placeholder="Search your stats .."
+                      placeholder="Your nickname..."
                       aria-label="Search"
                       aria-describedby="button-addon3"
                     />
@@ -142,7 +142,7 @@ const Nav = () => {
                     type="search"
                     id="usernameSearch"
                     className="form-control relative flex-auto min-w-0 block w-72 px-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    placeholder="Search your stats .."
+                    placeholder="Your nickname..."
                     aria-label="Search"
                     aria-describedby="button-addon3"
                   />
